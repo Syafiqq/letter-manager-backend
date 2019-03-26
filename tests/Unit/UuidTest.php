65f44dc10532a03e0900472ce87cce39c90bc606
Syafiq <syafiq.rezpector@gmail.com>
2019-03-27 04:14:21 +0700

    Add UUID Test

---
+++ tests/Unit/UuidTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 27 March 2019, 4:07 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+class UuidTest extends TestCase
+{
+    public function testUuid1()
+    {
+        $this->assertTrue(true);
+        foreach (range(0, 100) as $_)
+        {
+            try
+            {
+                echo \Ramsey\Uuid\Uuid::uuid1()->toString() . "\n";
+            }
+            catch (Exception $e)
+            {
+                echo $e->getMessage();
+            }
+        }
+    }
+
+}
+
+?>
