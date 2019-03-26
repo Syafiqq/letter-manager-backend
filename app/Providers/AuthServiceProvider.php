2a29273331bf52207f2bbc174c458466cf670f29
Syafiq <syafiq.rezpector@gmail.com>
2019-03-26 20:33:24 +0700

    Update auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Eloquents\User;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -30,10 +29,7 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token'))
-            {
-                return User::where('api_token', $request->input('api_token'))->first();
-            }
+            return app('auth')->setRequest($request)->user();
         });
     }
 }
