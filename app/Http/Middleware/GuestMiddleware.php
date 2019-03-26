3dbccd06cbff1d4ab64b495d48cba34fddeebbd9
Syafiq <syafiq.rezpector@gmail.com>
2019-03-26 21:39:44 +0700

    Add Guest Middleware

---
+++ app/Http/Middleware/GuestMiddleware.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 26 March 2019, 9:38 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Http\Middleware;
+
+
+use App\Model\Popo\PopoMapper;
+use App\Model\Util\HttpStatus;
+use Closure;
+use Tymon\JWTAuth\Http\Middleware\Check;
+use Tymon\JWTAuth\JWTAuth;
+
+class GuestMiddleware extends Check
+{
+    /**
+     * RedirectIfAuthenticated constructor.
+     * @param JWTAuth $auth
+     */
+    public function __construct(JWTAuth $auth)
+    {
+        parent::__construct($auth);
+    }
+
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($this->auth->parser()->setRequest($request)->hasToken())
+        {
+            try
+            {
+                if ($this->auth->parseToken()->authenticate() != null)
+                {
+                    return response()->json(PopoMapper::alertResponse(HttpStatus::FORBIDDEN, 'Can\'t access this page'), HttpStatus::FORBIDDEN);
+                }
+            }
+            catch (\Exception $_)
+            {
+                //
+            }
+        }
+
+        return $next($request);
+    }
+}
+
+?>
