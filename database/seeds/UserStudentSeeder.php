9e3f16253025ad0433eeef4ceaf8b4a68a57cf24
Syafiq <syafiq.rezpector@gmail.com>
2019-03-28 05:50:45 +0700

    Add User Seeder

---
+++ database/seeds/UserStudentSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UserStudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    public function run()
+    {
+        $inputFileName = __DIR__ . '/../vault/UserVault.csv';
+        $reader        = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+        $reader->setDelimiter(';');
+        $reader->setReadEmptyCells(true);
+        $reader->setReadDataOnly(true);
+        $spreadsheet = $reader->load($inputFileName);
+        $worksheet   = $spreadsheet->setActiveSheetIndex(0);
+        foreach ($worksheet->getRowIterator() as $row)
+        {
+            $val = [];
+            foreach ($row->getCellIterator('A', 'L') as $cell)
+            {
+                $val[] = $cell->getValue();
+            }
+            $user                     = new \App\Eloquents\User();
+            $user->{'id'}             = $val[0];
+            $user->{'credential'}     = $val[1];
+            $user->{'email'}          = $val[2];
+            $user->{'name'}           = $val[3];
+            $user->{'gender'}         = $val[4];
+            $user->{'role'}           = $val[5];
+            $user->{'stamp'}          = $val[6];
+            $user->{'avatar'}         = $val[7];
+            $user->{'password'}       = is_null($val[8]) ? null : app('hash')->make($val[8], []);
+            $user->{'remember_token'} = $val[9];
+            $user->{'created_at'}     = is_null($val[10]) ? null : Carbon::createFromFormat('Y-m-d H:i:s', $val[10]);
+            $user->{'updated_at'}     = is_null($val[11]) ? null : Carbon::createFromFormat('Y-m-d H:i:s', $val[11]);
+            $user->save();
+        }
+
+    }
+}
