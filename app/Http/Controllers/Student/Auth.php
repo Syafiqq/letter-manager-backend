c8f786cd0a3d853274c669d83df16d3485679e17
Syafiq <syafiq.rezpector@gmail.com>
2019-03-28 05:58:18 +0700

    Add Paramater

--- app/Http/Controllers/Student/Auth.php
+++ app/Http/Controllers/Student/Auth.php
@@ -3,12 +3,13 @@
 use App\Http\Controllers\BaseAuth;
 use Illuminate\Hashing\HashManager;
 use Tymon\JWTAuth\Factory;
+use Tymon\JWTAuth\JWTAuth;
 
 class Auth extends BaseAuth
 {
-    public function __construct(Factory $factory, HashManager $hashManager)
+    public function __construct(Factory $factory, HashManager $hashManager, JWTAuth $jwtAuth)
     {
-        parent::__construct($factory, $hashManager);
+        parent::__construct($factory, $hashManager, $jwtAuth);
         $this->role = 'student';
     }
 
