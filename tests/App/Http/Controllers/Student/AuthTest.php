566b6afda5bfcf4217867785838c9d8dda07278d
Syafiq <syafiq.rezpector@gmail.com>
2019-03-30 12:39:23 +0700

    Add testLoginWithRightData

--- tests/App/Http/Controllers/Student/AuthTest.php
+++ tests/App/Http/Controllers/Student/AuthTest.php
@@ -9,24 +9,37 @@
 
 class AuthTest extends ControllerTestCase
 {
-    const serverParam = [
-        'CONTENT_TYPE' => 'application/json',
-        'ACCEPT' => 'application/json'
-    ];
-
     public function testLoginWithEmptyData()
     {
         $this->expectException(\Illuminate\Validation\ValidationException::class);
 
-        $request    = $this->createRequest(
+        $request    = $this->createJsonRequest(
             'POST',
             null,
-            '/student/auth/login',
-            self::serverParam
+            '/student/auth/login'
+        );
+        $controller = $this->app->make(\App\Http\Controllers\Student\Auth::class);
+
+        $controller->postLogin($request);
+
+        $this->assertNotNull($controller);
+    }
+
+    public function testLoginWithRightData()
+    {
+        $request    = $this->createJsonRequest(
+            'POST',
+            [
+                'credential' => '10001',
+                'password' => '12345678',
+                'role' => 'student'
+            ],
+            '/student/auth/login'
         );
         $controller = $this->app->make(\App\Http\Controllers\Student\Auth::class);
 
         $response = $controller->postLogin($request);
+        $this->assertEquals(200, $response->status());
 
         $this->assertNotNull($controller);
     }
