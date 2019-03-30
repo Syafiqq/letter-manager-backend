d16962096ad2f6b53281e151cd5d5a188122cc30
Syafiq <syafiq.rezpector@gmail.com>
2019-03-30 16:13:10 +0700

    Add route Helper

---
+++ app/Helpers/routeHelper.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 30 March 2019, 4:10 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+if (!function_exists('path_route'))
+{
+    /**
+     * Generate a URL to a named route.
+     *
+     * @param  string $name
+     * @param  array $parameters
+     * @param  bool|null $secure
+     * @return string
+     */
+    function path_route($name, $parameters = [], $secure = null)
+    {
+        $domain = env('APP_URL');
+        if ($secure == true)
+        {
+            str_replace('http', 'https', $domain);
+        }
+        $route = app('url')->route($name, $parameters, $secure);
+
+        return str_replace($domain, '', $route);
+    }
+}
+
+?>
