7514e45725c0bd4b716097e48a1cf247bfcf958b
Syafiq <syafiq.rezpector@gmail.com>
2019-03-30 11:20:38 +0700

    Add doc

--- tests/App/Http/Controllers/ControllerTestCase.php
+++ tests/App/Http/Controllers/ControllerTestCase.php
@@ -9,9 +9,20 @@
 
 class ControllerTestCase extends TestCase
 {
+    /**
+     * @param string $uri The URI
+     * @param string $method The HTTP method
+     * @param array $parameters The query (GET) or request (POST) parameters
+     * @param array $cookies The request cookies ($_COOKIE)
+     * @param array $files The request files ($_FILES)
+     * @param array $server The server parameters ($_SERVER)
+     * @param string|resource|null $content The raw body data
+     *
+     * @return \Illuminate\Http\Request
+     */
     protected function createRequest(
         string $method,
-        string $content,
+        $content,
         string $uri = '/test',
         array $server = ['CONTENT_TYPE' => 'application/json'],
         array $parameters = [],
