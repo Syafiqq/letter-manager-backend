c0386a1d6365d53893db8895e436f0caa1c094a8
Syafiq <syafiq.rezpector@gmail.com>
2019-03-30 12:35:09 +0700

    Add json Request

--- tests/App/Http/Controllers/ControllerTestCase.php
+++ tests/App/Http/Controllers/ControllerTestCase.php
@@ -23,7 +23,7 @@ class ControllerTestCase extends TestCase
     protected function createRequest(
         string $method,
         $content,
-        string $uri = '/test',
+        string $uri,
         array $server = ['CONTENT_TYPE' => 'application/json'],
         array $parameters = [],
         array $cookies = [],
@@ -31,6 +31,42 @@ class ControllerTestCase extends TestCase
     )
     {
         return \Illuminate\Http\Request::createFromBase(\Symfony\Component\HttpFoundation\Request::create($uri, $method, $parameters, $cookies, $files, $server, $content));
+    }
+
+    /**
+     * @param string $uri The URI
+     * @param string $method The HTTP method
+     * @param array $parameters The query (GET) or request (POST) parameters
+     * @param array $cookies The request cookies ($_COOKIE)
+     * @param array $files The request files ($_FILES)
+     * @param array $server The server parameters ($_SERVER)
+     * @param string|resource|null $content The raw body data
+     *
+     * @return \Illuminate\Http\Request
+     */
+    protected function createJsonRequest(
+        string $method,
+        $content,
+        string $uri,
+        array $server = [
+            'CONTENT_TYPE' => 'application/json',
+            'ACCEPT' => 'application/json',
+        ],
+        array $parameters = [],
+        array $cookies = [],
+        array $files = []
+    )
+    {
+        if (is_null($content))
+        {
+            $content = [];
+        }
+        if (!is_array($content))
+        {
+            $content = [$content];
+        }
+
+        return $this->createRequest($method, $content, $uri, $server, $parameters, $cookies, $files);
     }
 }
 
