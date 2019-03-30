1c88d0f60bf2e2a55e7802972a8b1ebf58562c1f
Syafiq <syafiq.rezpector@gmail.com>
2019-03-30 11:34:37 +0700

    Add Auth Test

---
+++ tests/App/Http/Controllers/Student/AuthTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 30 March 2019, 10:52 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+class AuthTest extends ControllerTestCase
+{
+    const serverParam = [
+        'CONTENT_TYPE' => 'application/json',
+        'ACCEPT' => 'application/json'
+    ];
+
+    public function testLogin()
+    {
+        $this->expectException(\Illuminate\Validation\ValidationException::class);
+
+        $request    = $this->createRequest(
+            'POST',
+            null,
+            '/student/auth/login',
+            self::serverParam
+        );
+        $controller = $this->app->make(\App\Http\Controllers\Student\Auth::class);
+
+        $response = $controller->postLogin($request);
+
+        $this->assertNotNull($controller);
+    }
+}
+
+?>
