4ee2dab8e4811efb5504ce33eac44187b550f61e
Syafiq <syafiq.rezpector@gmail.com>
2019-03-31 18:07:30 +0700

    Add Valid Auth Recovery Middleware

---
+++ app/Http/Middleware/ValidAuthRecoveryMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 31 March 2019, 6:05 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Http\Middleware;
+
+
+use App\Eloquents\User;
+use App\Model\Popo\PopoMapper;
+use App\Model\Util\HttpStatus;
+use Closure;
+
+class ValidAuthRecoveryMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $request->input('token', '@)(I$)!@$');
+        $user  = User::where('lost_password', '=', $token)->first();
+        if (is_null($user))
+        {
+            return response()->json(PopoMapper::alertResponse(HttpStatus::NOT_FOUND, 'Page Not Found'), HttpStatus::NOT_FOUND);
+        }
+
+        return $next($request);
+    }
+}
+
+?>
