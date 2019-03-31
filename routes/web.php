42659682a5fb29b33dd3b17fc12f071ec954ebe3
Syafiq <syafiq.rezpector@gmail.com>
2019-03-31 18:52:30 +0700

    Finishing auth route

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,33 @@
 |
 */
 
+
+use App\Model\Popo\PopoMapper;
+use App\Model\Util\HttpStatus;
+
 /** @var \Laravel\Lumen\Routing\Router $router */
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return response()->json(PopoMapper::jsonResponse(HttpStatus::OK, '', [
+        'version' => $router->app->version()
+    ]), HttpStatus::OK);
+});
+$router->post('/', ['middleware' => ['c.jwt.auth'], function () use ($router) {
+    return response()->json(PopoMapper::jsonResponse(HttpStatus::OK, '', [
+        'version' => $router->app->version()
+    ]), HttpStatus::OK);
+}]);
+
+$router->group(['namespace' => 'Student', 'prefix' => '/student', 'middleware' => ['registered.role']], function () use ($router) {
+    $router->group(['prefix' => '/auth'], function () use ($router) {
+        $router->post('/refresh', ['middleware' => ['c.jwt.refresh'], 'uses' => 'Auth@postRefresh', 'as' => 'student.auth.refresh.post']);
+        $router->group(['middleware' => ['guest']], function () use ($router) {
+            $router->post('/login', ['uses' => 'Auth@postLogin', 'as' => 'student.auth.login.post']);
+            $router->post('/register', ['uses' => 'Auth@postRegister', 'as' => 'student.auth.register.post']);
+            $router->post('/lost', ['uses' => 'Auth@postLost', 'as' => 'student.auth.lost.post']);
+            $router->patch('/recover', ['middleware' => ['valid.auth.recovery'], 'uses' => 'Auth@patchRecover', 'as' => 'student.auth.recover.patch']);
+        });
+    });
+    $router->group(['middleware' => ['c.jwt.auth:student']], function () use ($router) {
+        $router->post('/auth/logout', ['uses' => 'Auth@postLogout', 'as' => 'student.auth.logout.post']);
+    });
 });
