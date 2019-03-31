5afd651a99cf1e2c348651263a871f16f33e5856
Syafiq <syafiq.rezpector@gmail.com>
2019-03-31 09:43:35 +0700

    Add coupon seeder

---
+++ database/seeds/CouponSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Carbon\Carbon;
+
+class CouponSeeder extends RollbackAbleSeeder
+{
+    /**
+     * Run the database seeds
+     *
+     * @return void
+     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    public function run()
+    {
+        $inputFileName = __DIR__ . '/../vault/CouponVault.csv';
+        $reader        = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+        $reader->setDelimiter(';');
+        $reader->setReadEmptyCells(true);
+        $reader->setReadDataOnly(true);
+        $spreadsheet = $reader->load($inputFileName);
+        $worksheet   = $spreadsheet->setActiveSheetIndex(0);
+        foreach ($worksheet->getRowIterator() as $row)
+        {
+            $val = [];
+            foreach ($row->getCellIterator('A', 'E') as $cell)
+            {
+                $val[] = $cell->getValue();
+            }
+            $model                 = new \App\Eloquents\Coupon();
+            $model->{'id'}         = $val[0];
+            $model->{'coupon'}     = $val[1];
+            $model->{'assignee'}   = $val[2];
+            $model->{'usage'}      = $val[3];
+            $model->{'created_at'} = is_null($val[4]) ? null : Carbon::createFromFormat('Y-m-d H:i:s', $val[4]);
+            $model->save();
+        }
+
+    }
+
+
+    function roll()
+    {
+        \App\Eloquents\Coupon::query()->delete();
+    }
+}
