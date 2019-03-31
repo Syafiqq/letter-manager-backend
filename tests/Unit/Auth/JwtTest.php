b44df3824645625e63dc5d95cca3bdce0966ad95
Syafiq <syafiq.rezpector@gmail.com>
2019-03-31 12:22:10 +0700

    Add JWT Test

---
+++ tests/Unit/Auth/JwtTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 31 March 2019, 12:21 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+class JwtTest extends TestCase
+{
+
+}
+
+?>
