e33179eb099c3cd88beeb58f4799a7e68b13a878
Syafiq <syafiq.rezpector@gmail.com>
2019-03-31 09:01:20 +0700

    Add coupon model

---
+++ app/Eloquents/Coupon.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 31 March 2019, 8:47 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Eloquents;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Coupon extends Model
+{
+    const table = 'coupons';
+    const usages = User::roles;
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+    /**
+     * @var string
+     */
+    protected $table = Coupon::table;
+    /**
+     * @var array
+     */
+    protected $dates = [
+        'created_at'
+    ];
+    /**
+     * @var array
+     */
+    protected $guarded = [
+        'id',
+        'usage',
+        'created_at',
+    ];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'coupon'
+    ];
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    protected $casts = [
+        'id' => 'string',
+        'assignee' => 'string'
+    ];
+    public $incrementing = false;
+
+    /**
+     * @return mixed
+     */
+    public function getRouteKey()
+    {
+        return $this->{'id'};
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateFormat()
+    {
+        return 'Y-m-d H:i:s';
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function users()
+    {
+        return $this->belongsTo('\App\Eloquents\User', 'assignee', 'id');
+    }
+
+    public function getHumanReadableUsage()
+    {
+        if (empty($this->{'usage'}))
+        {
+            return null;
+        }
+        else
+        {
+            switch ($this->{'usage'})
+            {
+                case 'student':
+                    return 'Student';
+            }
+        }
+    }
+}
+
+?>
