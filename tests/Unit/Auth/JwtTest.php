74fcf03fa2cc6beb1bd9c974edec6291808340c4
Syafiq <syafiq.rezpector@gmail.com>
2019-03-31 12:26:34 +0700

    Add test encode

--- tests/Unit/Auth/JwtTest.php
+++ tests/Unit/Auth/JwtTest.php
@@ -1,4 +1,7 @@
 <?php
+
+use App\Model\Util\ClaimTable;
+
 /**
  * This <letter-manager-backend> project created by :
  * Name         : syafiq
@@ -6,10 +9,31 @@
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
-
 class JwtTest extends TestCase
 {
+    public function test_encode()
+    {
+        $this->assertTrue(true);
+        /** @var \Tymon\JWTAuth\Factory $factory */
+        $factory = $this->app->make('tymon.jwt.payload.factory');
+        /** @var \Tymon\JWTAuth\JWTAuth $auth */
+        $auth   = $this->app->make('tymon.jwt.auth');
+        $claims = [
+            ClaimTable::AUDIENCE => 'audience', //Audience of the token
+            ClaimTable::ISSUER => 'issuer', // Issuer of the token
+            ClaimTable::SUBJECT => 'subject', // Subject of the token
+            ClaimTable::ISSUED_AT => time(), // Time when JWT was issued.
+            ClaimTable::EXPIRATION => time() + 60 * 60, // Expiration time
+            ClaimTable::AUTH_STAMP => 'stamp',
+            ClaimTable::SESSION => 'session',
+            ClaimTable::ROLE => 'role',
+        ];
 
+        $payload = $factory->customClaims($claims)->make();
+
+        $encode = $auth->manager()->encode($payload);
+        echo $encode->get();
+    }
 }
 
 ?>
