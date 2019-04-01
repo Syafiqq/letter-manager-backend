1626928811f8fbff94ccaf5b8bd8543e8caaf2e8
Syafiq <syafiq.rezpector@gmail.com>
2019-04-01 21:28:01 +0700

    Add letter

---
+++ app/Eloquents/Letter.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 01 April 2019, 9:00 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Eloquents;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Letter extends Model
+{
+    const table = 'letters';
+    const letterKind = [
+        'incoming',
+        'outgoing'
+    ];
+
+    /**
+     * @var bool
+     */
+    public $timestamps = true;
+    /**
+     * @var string
+     */
+    protected $table = Letter::table;
+    /**
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'date'
+    ];
+    /**
+     * @var array
+     */
+    protected $guarded = [
+        'id',
+        'issuer',
+        'number',
+        'created_at',
+    ];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'code',
+        'index',
+        'subject',
+        'date',
+        'kind',
+        'file',
+        'updated_at'
+    ];
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    protected $casts = [
+        'id' => 'string',
+        'issuer' => 'string'
+    ];
+    public $incrementing = false;
+
+    /**
+     * @return mixed
+     */
+    public function getRouteKey()
+    {
+        return $this->{'id'};
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateFormat()
+    {
+        return 'Y-m-d H:i:s';
+    }
+}
+
+?>
