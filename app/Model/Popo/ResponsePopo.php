12daf1c7d009c2657360b8edba0abd247d443206
Syafiq <syafiq.rezpector@gmail.com>
2019-04-23 23:24:43 +0700

    Add response popo

---
+++ app/Model/Popo/ResponsePopo.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 23 April 2019, 11:22 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Model\Popo;
+
+
+class ResponsePopo
+{
+    private $code;
+    private $status;
+    private $data;
+    private $notify;
+    private $alert;
+
+    /**
+     * ResponsePopo constructor.
+     * @param int $code
+     * @param string $status
+     * @param array $data
+     * @param array $notify
+     * @param array $alert
+     */
+    public function __construct($code = 200, $status = '', $data = [], $notify = [], $alert = [])
+    {
+        $this->code   = $code;
+        $this->status = $status;
+        $this->data   = $data;
+        $this->notify = $notify;
+        $this->alert  = $alert;
+    }
+}
+
+?>
