fc81fec906ef51818707a05d5c13c13708b8ed2c
Syafiq <syafiq.rezpector@gmail.com>
2019-04-23 23:11:06 +0700

    Add notify and alert sanitation

--- app/Model/Popo/PopoMapper.php
+++ app/Model/Popo/PopoMapper.php
@@ -26,7 +26,12 @@ class PopoMapper
      */
     static function notifResponse($code = 200, $status = 'Empty Status', $data = [], $notify = [], $alert = [])
     {
-        return self::jsonResponse($code, $status, $data, array_merge($notify, [$status]), $alert);
+        $notify = array_merge($notify, [$status]);
+
+        $notify = self::sanitizeNotify($notify);
+        $alert  = self::sanitizeAlert($alert);
+
+        return self::_jsonResponse($code, $status, $data, $notify, $alert);
     }
 
     /**
@@ -39,7 +44,10 @@ class PopoMapper
      */
     static function jsonResponse($code = 200, $status = 'Empty Status', $data = [], $notify = [], $alert = [])
     {
-        return compact('code', 'status', 'data', 'notify', 'alert');
+        $notify = self::sanitizeNotify($notify);
+        $alert  = self::sanitizeAlert($alert);
+
+        return self::_jsonResponse($code, $status, $data, $notify, $alert);
     }
 
     /**
@@ -52,7 +60,78 @@ class PopoMapper
      */
     static function alertResponse($code = 200, $status = 'Empty Status', $data = [], $notify = [], $alert = [])
     {
-        return self::jsonResponse($code, $status, $data, $notify, array_merge($alert, [$status]));
+        $alert = array_merge($alert, [$status]);
+
+        $notify = self::sanitizeNotify($notify);
+        $alert  = self::sanitizeAlert($alert);
+
+        return self::_jsonResponse($code, $status, $data, $notify, $alert);
+    }
+
+    private static function sanitizeNotify(array $notify)
+    {
+        if (count($notify) <= 0)
+        {
+            return $notify;
+        }
+
+        $_notify = [];
+        foreach ($notify as &$v)
+        {
+            if (is_array($v))
+            {
+                array_push($_notify, $v);
+            }
+            else
+            {
+                array_push($_notify, [
+                    'message' => $v,
+                    'level' => 'info'
+                ]);
+            }
+        }
+
+        return $_notify;
+    }
+
+    private static function sanitizeAlert(array $alert)
+    {
+        if (count($alert) <= 0)
+        {
+            return $alert;
+        }
+
+        $_alert = [];
+        foreach ($alert as &$v)
+        {
+            if (is_array($v))
+            {
+                array_push($_alert, $v);
+            }
+            else
+            {
+                array_push($_alert, [
+                    'message' => $v,
+                    'level' => 'info',
+                    'duration' => -1
+                ]);
+            }
+        }
+
+        return $_alert;
+    }
+
+    /**
+     * @param int $code
+     * @param string $status
+     * @param array $data
+     * @param array $notify
+     * @param array $alert
+     * @return array
+     */
+    private static function _jsonResponse(int $code, string $status, array $data, array $notify, array $alert)
+    {
+        return compact('code', 'status', 'data', 'notify', 'alert');
     }
 }
 
