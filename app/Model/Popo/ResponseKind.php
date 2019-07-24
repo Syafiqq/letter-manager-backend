1ca100a315a3608218e2b83901ff06206dc6a670
Syafiq <syafiq.rezpector@gmail.com>
2019-07-25 06:22:54 +0700

    Add response kind

---
+++ app/Model/Popo/ResponseKind.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 25 July 2019, 6:19 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Model\Popo;
+
+
+class ResponseKind
+{
+    public const SUCCESS = 'success';
+    public const INFO = 'info';
+    public const WARNING = 'warning';
+    public const DANGER = 'danger';
+}
+
+?>
