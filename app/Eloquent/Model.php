d96cef0df7835f210af1b1076571bfce147819a5
Syafiq <syafiq.rezpector@gmail.com>
2019-07-25 22:36:03 +0700

    Add model

---
+++ app/Eloquent/Model.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This <letter-manager-backend> project created by :
+ * Name         : syafiq
+ * Date / Time  : 25 July 2019, 6:58 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Eloquent;
+
+
+use Illuminate\Database\Query\Builder;
+
+/**
+ *
+ * @method static Builder|static where($column, $operator = null, $value = null, $boolean = 'and')
+ * @method static Builder|static insert(array $values)
+ * @method static Builder|static take($value)
+ */
+class Model extends \Illuminate\Database\Eloquent\Model
+{
+
+}
+
+?>
